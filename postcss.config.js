2b7466b553473310fc2ac3c990e1924105067282
Ha Quanq <quangha0120010@gmail.com>
2025-07-14 19:58:03 +0700

    Add Vite, PostCSS, Prettier

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/** @type {import('postcss-load-config').Config} */
+export default {
+    plugins: {
+        "postcss-nested": {},
+        "postcss-import": {},
+        "postcss-preset-env": {},
+        "postcss-custom-media": {},
+        autoprefixer: {},
+        cssnano: {},
+    },
+};
