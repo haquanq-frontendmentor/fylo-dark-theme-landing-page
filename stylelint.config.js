4558dd839a546f2e514f5edbcd966d781568b59f
Ha Quanq <quangha0120010@gmail.com>
2025-07-15 10:22:05 +0700

    Update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,11 @@
 /** @type {import('stylelint').Config} */
 export default {
     extends: "stylelint-config-standard",
+    rules: {
+        "font-family-name-quotes": "always-unless-keyword",
+        "custom-property-empty-line-before": null,
+        "color-function-notation": "legacy",
+        "hue-degree-notation": "number",
+        "import-notation": "string",
+    },
 };
