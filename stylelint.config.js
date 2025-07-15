a440cfe7881fc9797532ec829cef44827d1e21ea
Ha Quanq <quangha0120010@gmail.com>
2025-07-15 19:33:47 +0700

    Update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,12 +4,13 @@ export default {
     rules: {
         "custom-property-empty-line-before": null,
         "declaration-block-no-redundant-longhand-properties": null,
+        "declaration-block-no-duplicate-properties": true,
 
         "font-family-name-quotes": "always-unless-keyword",
         "color-function-notation": "legacy",
         "hue-degree-notation": "number",
         "import-notation": "string",
-        "at-rule-empty-line-before": ["always", { ignore: ["blockless-after-blockless"] }],
+        "at-rule-empty-line-before": ["always", { ignore: ["blockless-after-blockless", "after-comment"] }],
         "media-feature-range-notation": "prefix",
     },
 };
