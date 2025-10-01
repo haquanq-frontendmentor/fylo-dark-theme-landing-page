1400cc8faca63600d3b96379d6390fef97173016
Ha Quanq <quangha0120010@gmail.com>
2025-10-02 06:44:53 +0700

    Migrate to SASS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import postcssPresetEnv from "postcss-preset-env";
+import postcssSortMediaQueries from "postcss-sort-media-queries";
 
 export default defineConfig({
     root: "src/",
     build: { outDir: "../dist/", emptyOutDir: true },
     base: "/fylo-dark-theme-landing-page/",
+    css: {
+        postcss: {
+            plugins: [postcssPresetEnv({ autoprefixer: true }), postcssSortMediaQueries()],
+        },
+    },
 });
